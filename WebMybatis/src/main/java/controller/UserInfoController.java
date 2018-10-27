ab2ffe5ca7f24b1505c94376f87a666c6ea55a17
lxf <1092758202@qq.com>
2018-10-27 14:28:45 +0800

    add the demo

---
+++ WebMybatis/src/main/java/controller/UserInfoController.java
@@ -0,0 +1,36 @@
+package controller;
+
+import domain.UserInfo;
+import service.impl.UserServiceImpl;
+
+import java.util.Date;
+import java.util.List;
+
+public class UserInfoController {
+
+    public static void main(String[] args) {
+
+        UserServiceImpl userService=new UserServiceImpl();
+
+/*        for (int i=0;i<10;i++){
+            UserInfo userInfo=new UserInfo();
+            userInfo.setUserName("admin"+String.valueOf(i));
+            userInfo.setAge(i%3);
+            long time=System.currentTimeMillis();
+            Date date=new Date(time);
+            userInfo.setSex(i%2);
+            userInfo.setCreateDate(date);
+            userService.addUser(userInfo);
+        }*/
+
+/*List<UserInfo> userInfos= userService.getUsersByParams(0,1,0);
+
+        for (UserInfo userInfo:userInfos) {
+            System.out.println(userInfo.getUserName());
+        }*/
+
+UserInfo userInfo= userService.getUserByUsername("admin9");
+userInfo.setUserName("admin99");
+userService.updateUser(userInfo);
+    }
+}
---
+++ WebMybatis/src/main/java/service/impl/UserServiceImpl.java
@@ -0,0 +1,62 @@
+package service.impl;
+
+
+
+import dao.UserInfoMapper;
+import domain.UserInfo;
+import domain.UserInfoExample;
+import org.apache.ibatis.session.SqlSession;
+import service.UserService;
+import util.MybatisUtil;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public void addUser(UserInfo userInfo) {
+        SqlSession session= MybatisUtil.getSession();
+     UserInfoMapper userInfoMapper= session.getMapper(UserInfoMapper.class);
+     userInfoMapper.insert(userInfo);
+     session.commit();
+
+
+    }
+
+    @Override
+    public int deleteUserByUsername(String userName) {
+
+       SqlSession sqlSession=MybatisUtil.getSession();
+        UserInfoMapper userInfoMapper= sqlSession.getMapper(UserInfoMapper.class);
+       int num= userInfoMapper.deleteByUsername(userName);
+        sqlSession.commit();
+        return num;
+    }
+
+    @Override
+    public void updateUser(UserInfo userInfo) {
+
+        SqlSession sqlSession=MybatisUtil.getSession();
+        UserInfoMapper userInfoMapper= sqlSession.getMapper(UserInfoMapper.class);
+        userInfoMapper.updateByPrimaryKey(userInfo);
+        sqlSession.commit();
+    }
+
+    @Override
+    public UserInfo getUserByUsername(String username) {
+        SqlSession sqlSession=MybatisUtil.getSession();
+        UserInfoMapper userInfoMapper= sqlSession.getMapper(UserInfoMapper.class);
+        return userInfoMapper.selectByUsername(username);
+
+    }
+
+    @Override
+    public List<UserInfo> getUsersByParams(Integer ageBegin,Integer ageEnd, Integer sex) {
+        SqlSession sqlSession=MybatisUtil.getSession();
+        UserInfoMapper userInfoMapper= sqlSession.getMapper(UserInfoMapper.class);
+        UserInfoExample userInfoExample=new UserInfoExample();
+        userInfoExample.createCriteria().andAgeBetween(ageBegin,ageEnd).andSexEqualTo(sex);
+        return userInfoMapper.selectByExample(userInfoExample);
+    }
+
+
+}
